6729a597ecbd63c9f394e63a5f7eb8659aad11a6
João Souza <joaodealmedassouza@gmail.com>
2025-03-14 18:43:10 -0300

    upando exercicios

--- AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer1.java
+++ AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer1.java
@@ -2,6 +2,9 @@ package com.example.androidclass;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +24,39 @@ public class ViewExer1 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText edTextNome = findViewById(R.id.editTextNome);
+        EditText edTextIdade = findViewById(R.id.editTextIdade);
+        Button btnEnviar = findViewById(R.id.button);
+
+
+
+
+        btnEnviar.setOnClickListener( v -> {
+            String nome = edTextNome.getText().toString().trim();
+            String idadeStr = edTextIdade.getText().toString().trim();
+
+            // Verificando se os campos estão vazios
+            if (nome.isEmpty() || idadeStr.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    int idade = Integer.parseInt(idadeStr);
+
+                    if (idade >= 18) {
+                        Toast.makeText(this, nome + " você é maior de idade.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, nome + " você é menor de idade.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "Idade inválida!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
+
 
     public void voltar(View view){finish();}
 }
