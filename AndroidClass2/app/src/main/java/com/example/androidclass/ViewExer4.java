c03b7ec88f2364932a6fc927cfe9025a2f05f0b3
João Souza <joaodealmedassouza@gmail.com>
2025-03-19 01:52:52 -0300

    Updating Exercises

--- AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer4.java
+++ AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer4.java
@@ -2,6 +2,11 @@ package com.example.androidclass;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ViewExer4 extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class ViewExer4 extends AppCompatActivity {
+    private List<CheckBox> checkBoxList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,31 @@ public class ViewExer4 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //fazer o usuário digitar o nome na tela e, para cada letra do nome, um checkbox
+
+
+        LinearLayout checkBoxContainer = findViewById(R.id.checkBoxContainer);
+        EditText editTextnome = findViewById(R.id.editTextNomeEx4);
+        Button btnEnviar = findViewById(R.id.btnEnviarEx4);
+
+        btnEnviar.setOnClickListener(v -> {
+            String nomeCompleto = editTextnome.getText().toString().trim();
+            char[] letras = nomeCompleto.toCharArray();
+            List<String> opcoes = new ArrayList<>();
+
+            for (char letra : letras) {
+                opcoes.add(String.valueOf(letra));
+            }
+
+            for (String opcao : opcoes)
+            {
+                CheckBox checkBox = new CheckBox(this);
+                checkBox.setText(opcao);
+                checkBoxContainer.addView(checkBox);
+                checkBoxList.add(checkBox);
+            }
+        });
+
     }
     public void voltar(View view){finish();}
 
