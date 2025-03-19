4c01ddc2f3c482a3ca2959b12728a83528bd97c7
Aluno <aluno@facens.br>
2025-03-18 22:01:03 -0300

    Commiting exercises

--- AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer3.java
+++ AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer3.java
@@ -2,6 +2,10 @@ package com.example.androidclass;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +25,27 @@ public class ViewExer3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinner = findViewById(R.id.spinner);
+        String[] cores = {"Vermelho", "Verde", "Azul", "Amarelo", "Preto"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cores);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String corSelecionada = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "Cor escolhida: " + corSelecionada, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
     }
+
 
     public void voltar(View view){finish();}
 
