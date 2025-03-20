f650d841ede3201ca4f78b5b59eeebfb2326ea15
João Souza <joaodealmedassouza@gmail.com>
2025-03-19 22:40:56 -0300

    Fixing Exercises

--- AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer5.java
+++ AndroidClass2/app/src/main/java/com/example/androidclass/ViewExer5.java
@@ -1,13 +1,19 @@
 package com.example.androidclass;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class ViewExer5 extends AppCompatActivity {
 
@@ -21,6 +27,29 @@ public class ViewExer5 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        CheckBox cbLogin = findViewById(R.id.checkBoxLogin);
+        CheckBox cbModoEscuro = findViewById(R.id.checkBoxModoEscuro);
+        CheckBox cbNotificacao = findViewById(R.id.checkBoxNotif);
+        Button btnSalvar = findViewById(R.id.buttonSalvarPrefe);
+        ArrayList<String> opcoes = new ArrayList<>();
+
+
+        btnSalvar.setOnClickListener( v -> {
+            if (cbModoEscuro.isChecked()){
+                opcoes.add(cbModoEscuro.getText().toString());
+            }
+            if (cbNotificacao.isChecked()){
+                opcoes.add(cbNotificacao.getText().toString());
+            }
+            if (cbLogin.isChecked()){
+                opcoes.add(cbLogin.getText().toString());
+            }
+
+
+            Toast.makeText(this, "Preferências salvas: " + TextUtils.join(", ", opcoes), Toast.LENGTH_LONG).show();
+        });
+
+
     }
     public void voltar(View view){finish();}
 
